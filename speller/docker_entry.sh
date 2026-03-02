361b16cbc311af15e24811669056f90f1e9f2686
fluffy-marmot <10621013+fluffy-marmot@users.noreply.github.com>
2026-03-02 12:08:25 -0800

    add docker file, docker entry script, instructions for deps

---
+++ speller/docker_entry.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+# copy submission files that Flask has hopefully written to submission/ dir
+cp /submission/dictionary.c /speller/dictionary.c
+cp /submission/dictionary.h /speller/dictionary.h
+
+cd /speller
+make -B
+./speller texts/wordsworth.txt
